c8128eddf903cd9f074dd78fa36af2e3c5cbe543
Alik <Nigay_AO@bk.ru>
2022-02-08 18:04:46 +0300

    Initial Commit

---
+++ Homework 3.6/Homework_3_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_3_6App.swift
+//  Homework 3.6
+//
+//  Created by Alik Nigay on 08.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Homework_3_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
